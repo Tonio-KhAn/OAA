75b2afca45d7d2cb66dd1fc2212a95f6523e461f
unknown <tonio.d.boss@gmail.com>
2021-02-27 04:38:01 -0400

    web vitals

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
-reportWebVitals();
